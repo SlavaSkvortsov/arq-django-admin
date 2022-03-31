3b6c6dacfb93eb05e980cf221afc04ea6d6329f3
Christopher Bailey <cbailey@mort.is>
2022-03-31 01:28:30 -0400

    Adds queue_name option to get_redis

--- arq_admin/redis.py
+++ arq_admin/redis.py
@@ -1,13 +1,15 @@
 from contextlib import asynccontextmanager
+from email.policy import default
 from typing import AsyncGenerator
 
+from arq.constants import default_queue_name
 from arq.connections import ArqRedis, RedisSettings, create_pool
 from arq_admin.utils import is_redis_py
 
 
 @asynccontextmanager
-async def get_redis(setting: RedisSettings) -> AsyncGenerator[ArqRedis, None]:
-    redis = await create_pool(setting)
+async def get_redis(setting: RedisSettings, queue_name: str = default_queue_name) -> AsyncGenerator[ArqRedis, None]:
+    redis = await create_pool(setting, default_queue_name=queue_name)
     try:
         yield redis
     finally:
